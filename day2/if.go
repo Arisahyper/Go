6ba69f85f85901840d7d61af21d50c7776c82c9f
Aqua Minato <sweet4182691@gmail.com>
2022-01-02 17:01:56 +0900

    .

---
+++ day2/if.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func standarIf() {
+	num := 10
+	if num%2 == 0 {
+		fmt.Println("even")
+	} else if num == 0 {
+		fmt.Println("num == 0")
+	} else {
+		fmt.Println("odd")
+	}
+}
+
+func a() {
+
+}
+
+func main() {
+}
