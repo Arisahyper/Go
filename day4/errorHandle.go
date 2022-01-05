f9ee320cde2ed1a7eeaafbce5ab64e1ce4a3b03e
Aqua Minato <sweet4182691@gmail.com>
2022-01-05 17:14:09 +0900

    .

---
+++ day4/errorHandle.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./day4/errorHandle.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data := make([]byte, 100)
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(count)
+	fmt.Println(string(data))
+}
