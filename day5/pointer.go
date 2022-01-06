9088bda8baaec4e404f0f251d3ec8bc908e3391e
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 06:15:48 +0900

    .

--- day5/pointer.go
+++ day5/pointer.go
@@ -7,7 +7,14 @@ func main() {
 	fmt.Println(n)  // 100
 	fmt.Println(&n) // 変数nのメモリのアドレス
 
-	var p *int = &n
+	var p *int = &n // ポインタ型
 	fmt.Println(p)  // 変数nのメモリのアドレス / 上と同じ
 	fmt.Println(*p) // 変数nの値 / 変数nの値を参照する
 }
+
+func p(arg *int) int { // メモリの番地を渡す
+	return *arg + 1
+}
+
+// 番地指定したい時はポインタ型で渡す
+// 参照したい時は、&をつける
