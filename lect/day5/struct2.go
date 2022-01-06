adbf8d35c2bdedf73a5dd4c583d8668847e067fd
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 07:31:18 +0900

    .

---
+++ lect/day5/struct2.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func changeVertex(v Vertex) {
+	v.X = 1000
+}
+
+func changeVertex2(v *Vertex) {
+	v.X = 1000
+}
+
+func main() {
+	v := Vertex{1, 2}
+	changeVertex(v)
+	fmt.Println(v) // {1 2}	/ 値渡し
+
+	changeVertex2(&v)
+	fmt.Println(v) // {1000 2}	/ 参照渡し
+}
