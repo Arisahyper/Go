2c8a81a9ac3dcdadc7da1948b52bfc0d5d7ce01e
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 07:39:43 +0900

    .

--- lect/ex/ex3.go
+++ lect/ex/ex3.go
@@ -6,8 +6,12 @@ func main() {
 	var i int = 10 // int型
 	var p *int     // int型のポインタ / intポインタ型
 	p = &i         // 変数pに変数iのアドレスを代入
-	fmt.Println(p) // 0x10 / &iのアドレスが入ってる
+	fmt.Println(p) // 0x10 / iのアドレスが入ってる
 	var j int      // int型
 	j = *p         // jにアドレスであるpの中身を代入
-	fmt.Println(j) // 10 / *pの中身が入ってる
+	fmt.Println(j) // 10 / pの中身が入ってる
 }
+
+// -- メモ --
+// 実数を見たい時は「*」
+// アドレスを見たい時は「&」
