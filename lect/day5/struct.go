1cd05d8a23da58a6160d46672724868154f1c1fa
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 07:22:39 +0900

    .

--- lect/day5/struct.go
+++ lect/day5/struct.go
@@ -5,6 +5,7 @@ import "fmt"
 type Vertex struct {
 	X int
 	Y int
+	S string
 }
 
 func main() {
@@ -13,4 +14,13 @@ func main() {
 	fmt.Println(v.X, v.Y)   // 1 2
 	v.X = 100               // 代入
 	fmt.Println(v.X, v.Y)   // 100 2
+
+	v2 := Vertex{X: 1} // Y:0
+	fmt.Println(v2)    // {1 0}
+
+	v3 := Vertex{1, 2, "test"} // 省略可能
+	fmt.Println(v3)            // {1 2 test}
+
+	v4 := Vertex{}
+	fmt.Println(v4) // {0 0  }
 }
