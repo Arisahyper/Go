3a8a245ae598c543db584ab96812476e7d173a68
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 05:49:32 +0900

    gof

--- day5/pointer.go
+++ day5/pointer.go
@@ -4,11 +4,10 @@ import "fmt"
 
 func main() {
 	var n int = 100
-	fmt.Println(n)	// 100
-	fmt.Println(&n)	// 変数nのメモリのアドレス
+	fmt.Println(n)  // 100
+	fmt.Println(&n) // 変数nのメモリのアドレス
 
 	var p *int = &n
-
-	fmt.Println(p)	// 変数nのメモリのアドレス / 上と同じ
-	fmt.Println(*p)	// 変数nの値 / 変数nの値を参照する
+	fmt.Println(p)  // 変数nのメモリのアドレス / 上と同じ
+	fmt.Println(*p) // 変数nの値 / 変数nの値を参照する
 }
