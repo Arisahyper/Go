3f647fe2a2d1454cce1199e961b0c5a2e913d93d
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 06:25:01 +0900

    .

--- day5/new.go
+++ day5/new.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	var n *int = new(int) // ポインタを作成 / アドレスを先に確保する
+	fmt.Println(n)        // 0x0
+	fmt.Println(*n)       // 0
+	*n++                  // 足してみる
+	fmt.Println(*n)       // 1
+
+	var o *int
+	fmt.Println(o) 		// nil
+	// *o++           // panic: runtime error: invalid memory address or nil pointer dereference
+	// fmt.Println(o) // nil
 }
