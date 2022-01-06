24d73f5a5c58ceceb42364209a2b02915e5f27fc
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 05:49:14 +0900

    pointer

---
+++ day5/pointer.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int = 100
+	fmt.Println(n)	// 100
+	fmt.Println(&n)	// 変数nのメモリのアドレス
+
+	var p *int = &n
+
+	fmt.Println(p)	// 変数nのメモリのアドレス / 上と同じ
+	fmt.Println(*p)	// 変数nの値 / 変数nの値を参照する
+}
