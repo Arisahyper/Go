7d010107f25dbb189728aff056e68024fb6bac38
Aqua Minato <sweet4182691@gmail.com>
2022-01-07 07:37:27 +0900

    .

---
+++ lect/ex/ex3.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 10 // int型
+	var p *int     // int型のポインタ / intポインタ型
+	p = &i         // 変数pに変数iのアドレスを代入
+	fmt.Println(p) // 0x10 / &iのアドレスが入ってる
+	var j int      // int型
+	j = *p         // jにアドレスであるpの中身を代入
+	fmt.Println(j) // 10 / *pの中身が入ってる
+}
