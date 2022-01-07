cc4a640fa4513194f061599b87ae4542f19c2d55
Aqua Minato <sweet4182691@gmail.com>
2022-01-08 06:14:21 +0900

    .

---
+++ lect/day6/embedded.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+// 継承
+type Vertex3D struct {
+	Vertex
+	Z int
+}
+
+func (v Vertex3D) Area3D() int {
+	return v.X * v.Y * v.Z
+}
+
+func (v *Vertex3D) Scale3D(i int) {
+	v.X *= i
+	v.Y *= i
+	v.Z *= i
+}
+
+func New(x, y, z int) *Vertex3D {
+	return &Vertex3D{Vertex{x, y}, z}
+}
+
+func main() {
+	v := New(3, 4, 5)
+	fmt.Println(v)
+	fmt.Println(v.Area3D())
+	v.Scale3D(10)
+	fmt.Println(v)
+	fmt.Println(v.Area3D())
+}
