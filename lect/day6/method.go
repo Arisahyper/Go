e320ef7d8e56d879b62938e6e1e354b7931ab5d5
Aqua Minato <sweet4182691@gmail.com>
2022-01-08 06:02:28 +0900

    .

---
+++ lect/day6/method.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+// メソッド / Vertexに紐づく
+func (v Vertex) Area() int {
+	return v.X * v.Y
+}
+
+// 関数
+func Area(v Vertex) int {
+	return v.X * v.Y
+}
+
+func main() {
+	v := Vertex{3, 4}
+
+	fmt.Println(Area(v))
+	fmt.Println(v.Area())
+}
