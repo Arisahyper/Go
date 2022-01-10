ab9a728c8f5e6115a809eef3e2ea4948ee015225
Aqua Minato <sweet4182691@gmail.com>
2022-01-10 11:59:27 +0900

    .

---
+++ algorithm/234a.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func f(x float64) float64 {
+	return math.Pow(x, 2) + 2*x + 3
+}
+
+func main() {
+	var n float64
+	fmt.Scan(&n)
+
+	fmt.Println(int(f(f(f(n)+n)+f(f(n)))))
+}
+
+// 関数を作成 -> 繰り返し処理
