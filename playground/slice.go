efbebd82ab0ba437080a5063e8b42120faca24b4
Aqua Minato <sweet4182691@gmail.com>
2022-01-11 13:53:21 +0900

    .

--- playground/slice.go
+++ playground/slice.go
@@ -23,6 +23,11 @@ func main() {
 	list2 := []string{"piyo", "uga"}
 	sumList := append(list1, list2...)
 	fmt.Println(sumList)
+
+	// slice delete
+	list3 := []string{"1", "2", "3", "4", "5"}
+	list3 = append(list3[:1], list3[2:]...) // 1個目の要素と3個目以降の要素を足すことで削除をする
+	fmt.Println(list3)
 }
 
 
