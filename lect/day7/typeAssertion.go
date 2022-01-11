a996adbe738b07287e885f0fe40cbacdbd389d66
Aqua Minato <sweet4182691@gmail.com>
2022-01-11 09:20:33 +0900

    .

---
+++ lect/day7/typeAssertion.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// 引数 interface{} は、型を指定せず引数を取れる
+func typeBranch(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Println("int", v)
+	case string:
+		fmt.Println("string", v)
+	case bool:
+		fmt.Println("bool", v)
+	default:
+		fmt.Println("unknown", v)
+	}
+}
+
+func do(i interface{}){
+	// ii := i * 2 // error
+	ii := i.(int) * 2 // error
+	fmt.Println(ii)
+}
+
+func main() {
+	do(1)
+}
