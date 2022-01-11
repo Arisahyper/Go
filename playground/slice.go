17f389723199b19b293bc31055b6d8d7b11793eb
Aqua Minato <sweet4182691@gmail.com>
2022-01-11 13:43:12 +0900

    .

--- playground/slice.go
+++ playground/slice.go
@@ -6,4 +6,9 @@ func main() {
 	var h []string
 	h = append(h, "hoge")
 	fmt.Println(h)
+
+	s := []string{"hoge", "fuga", "piyo"}
+	fmt.Println(s)
+	fmt.Println(len(s))	// length
+	fmt.Println(cap(s)) // capacity
 }
