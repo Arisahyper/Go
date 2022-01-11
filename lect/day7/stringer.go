68aa8e18384b7fca8dac07fb6f166e487514328d
Aqua Minato <sweet4182691@gmail.com>
2022-01-11 09:37:10 +0900

    .

--- lect/day7/stringer.go
+++ lect/day7/stringer.go
@@ -1,10 +1,18 @@
 package main
 
+import "fmt"
+
 type Person struct {
 	name string
 	age  int
 }
 
-func main() {
+// これがあると出力が変わる
+func (p Person) String() string {
+	return fmt.Sprintf("%s: %d", p.name, p.age)
+}
 
+func main() {
+	mike := Person{"Mike", 20}
+	fmt.Println(mike)
 }
