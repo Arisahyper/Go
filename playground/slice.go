642dcb5afcdd1a3c176f9a09cfca6e70a7e5dffb
Aqua Minato <sweet4182691@gmail.com>
2022-01-11 13:46:38 +0900

    .

--- playground/slice.go
+++ playground/slice.go
@@ -11,4 +11,14 @@ func main() {
 	fmt.Println(s)
 	fmt.Println(len(s))	// length
 	fmt.Println(cap(s)) // capacity
+
+	// slice copy
+	list := []string{"hoge", "fuga", "piyo"}
+	dist := make([]string, len(list))
+	copy(dist, list)
+	fmt.Println(dist)
 }
+
+
+// スライス slice
+// 操作 コピー
