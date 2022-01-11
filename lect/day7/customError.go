bc6fb0a0696c716b78f6ad646178b74c6d4518d0
Aqua Minato <sweet4182691@gmail.com>
2022-01-11 09:49:56 +0900

    .

--- lect/day7/customError.go
+++ lect/day7/customError.go
@@ -4,14 +4,23 @@ import "fmt"
 
 // 自作エラー
 type UserNotFound struct {
+	UserName string
 }
 
-func myFunc() error {
-	return nil
+func (e *UserNotFound) Error() string {
+	return e.UserName + " is not found"
+}
+
+func myFunc(name string) error {
+	if name == "hoge" {
+		return nil // okならerrorはないのでnilを返す
+	}
+	return &UserNotFound{name} // errorならUserNotFoundを返す
 }
 
 func main() {
-	err := myFunc()
+	name := "hoge"
+	err := myFunc(name)
 	if err != nil {
 		fmt.Println(err)
 	}
