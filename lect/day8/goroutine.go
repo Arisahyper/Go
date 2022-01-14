0436b23091ab7c982c629a4e67817e3c626c6658
Aqua Minato <sweet4182691@gmail.com>
2022-01-14 21:24:12 +0900

    .

--- lect/day8/goroutine.go
+++ lect/day8/goroutine.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -12,9 +13,22 @@ func normal(s string) {
 	}
 }
 
+func gor(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for i := 0; i < 10; i++ {
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println(s)
+	}
+}
+
 func main() {
-	go normal("hoge")
-	normal("fuga")
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go gor("go routine", &wg)
+	normal("normal")
+
+	wg.Wait()
 }
 
 // goroutineが終わらなくてもプログラムが終わったら終わり
