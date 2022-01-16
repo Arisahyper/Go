32b3944befaa8dd966e77318835d2734d24ee760
Aqua Minato <sweet4182691@gmail.com>
2022-01-17 00:21:52 +0900

    .

--- lect/day8/bufferedChannels.go
+++ lect/day8/bufferedChannels.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	ch := make(chan int, 2) // チャネルを用意
 	ch <- 100               // チャネルにデータを送る
-	fmt.Println(len(ch))    // チャネルの要素数を表示
+	fmt.Println(len(ch))    // チャネルの要素数を表示 / 1
+	ch <- 200               // チャネルにデータを送る / もう入らない
+	fmt.Println(len(ch))    // チャネルの要素数を表示 / 2
 }
