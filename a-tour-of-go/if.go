aa8910fc5445dc0f93635def43f55e3abf2d6fb1
Aqua Minato <sweet4182691@gmail.com>
2022-01-18 04:35:33 +0900

    .

--- a-tour-of-go/if.go
+++ a-tour-of-go/if.go
@@ -12,7 +12,14 @@ func sqrt(x float64) (float64, error) {
 	return math.Sqrt(x), nil
 }
 
+func pow(x, n float64) float64 {
+	return math.Pow(x, n)
+}
+
 func main() {
 	ans, _ := sqrt(2)
+	fmt.Println(ans)
+
+	ans = pow(2, 3)
 	fmt.Println(ans)
 }
