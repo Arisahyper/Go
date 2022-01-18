bd02710dbc977046edf9b0882a531e4290e954a2
Aqua Minato <sweet4182691@gmail.com>
2022-01-18 17:35:12 +0900

    .

--- playground/flag.go
+++ playground/flag.go
@@ -5,13 +5,27 @@ import (
 	"fmt"
 )
 
-func main() {
-	flag.Parse()	
-	args := flag.Args()	// 引数を全部取得
+func standard() {
+	flag.Parse()
+	args := flag.Args() // 引数を全部取得
 	fmt.Println(args)
 
-	arg1 := flag.Arg(0)	// 引数を1つ目だけ取得
+	arg1 := flag.Arg(0) // 引数を1つ目だけ取得
 	fmt.Println(arg1)
 
-	fmt.Println(flag.Arg(0), flag.Arg(10))	// 何もなかった場合空文字列が返る
+	fmt.Println(flag.Arg(0), flag.Arg(10)) // 何もなかった場合空文字列が返る
+}
+
+func typeParse() {
+	var (
+		i = flag.Int("int", 0, "int flag")
+		s = flag.String("str", "default", "string flag")
+		b = flag.Bool("bool", false, "bool flag")
+	)
+	flag.Parse()
+	fmt.Println(*i, *s, *b)
+}
+
+func main() {
+
 }
