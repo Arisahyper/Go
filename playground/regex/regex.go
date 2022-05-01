9bdbd7b93007a178dc194eb8cb8f9011f883cb1a
Aqua Minato <sweet4182691@gmail.com>
2022-05-01 10:52:18 +0900

    .

--- playground/regex/regex.go
+++ playground/regex/regex.go
@@ -6,6 +6,10 @@ import (
 )
 
 func Match() {
+	pattern := regexp.MustCompile("^[0-9]{4}$")
+	ms := pattern.MatchString("2016")
+	fmt.Println(ms)
+
 	match, _ := regexp.MatchString("^[0-9]{4}$", "1234")
 	fmt.Println(match)
 }
