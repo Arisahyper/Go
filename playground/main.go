9dc30c974eb98d0bccd5f81139db91d1068d5ab1
Aqua Minato <sweet4182691@gmail.com>
2022-05-01 10:57:58 +0900

    .

--- playground/main.go
+++ playground/main.go
@@ -1,7 +1,4 @@
 package main
 
-import "playground/regex"
-
 func main() {
-	regex.Match()
 }
--- playground/regex/regex.go
+++ playground/regex/regex.go
@@ -6,9 +6,11 @@ import (
 )
 
 func Match() {
-	pattern := regexp.MustCompile("^[0-9]{4}$")
-	ms := pattern.MatchString("2016")
+	pattern := regexp.MustCompile(`^0\d{9,10}$`)
+	ms := pattern.MatchString("090-1234-5678")
+	ms2 := pattern.MatchString("09012345678")
 	fmt.Println(ms)
+	fmt.Println(ms2)
 
 	match, _ := regexp.MatchString("^[0-9]{4}$", "1234")
 	fmt.Println(match)
