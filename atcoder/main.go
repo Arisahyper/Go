1739489779bf33228193cfa84f34a7aa327a9f84
Yoshitsugu Tahara <arisahyper0000@gmail.com>
2022-07-09 13:41:45 +0900

    .

--- atcoder/main.go
+++ atcoder/main.go
@@ -1,18 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
-	var r, c int
-	var arr[2][2] int = [2][2]int{}
-	fmt.Scan(&r, &c)
-	fmt.Scan(&arr[0][0], &arr[0][1])
-	fmt.Scan(&arr[1][0], &arr[1][1])
+	var n int
+	fmt.Scan(&n)
 
-	fmt.Println(arr[r-1][c-1])
+	fmt.Println(strconv.Itoa(n)[1:])
 }
-
-/*
-	A11 A12
-	A21 A22
-*/
